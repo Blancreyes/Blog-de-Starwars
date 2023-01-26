2a781d6adbb2b72f13272ba79a4e6f6fe269dcd5
Blancreyes <luijbr83@gmail.com>
2023-01-26 21:38:48 +0000

    First Commit

---
+++ src/js/component/card.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Card = () => {
+	//the basename is used when your project is published in a subdirectory and not in the root of the domain
+	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	// const basename = process.env.BASENAME || "";
+
+	return (
+		<div className="card" style={{width: "18rem"}}>
+            <img src="../img/rigo-baby.jpg" className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            
+            <div className="col d-flex justify-content-around py-4">
+                <div>
+                    <button href="#" className="btn btn-outline-primary">Learn more!</button>
+                </div>
+                <div>
+                    <button href="#" className="btn btn-warning"><i className="fa fa-heart"></i></button>
+                </div>
+                            
+            </div>
+        </div>
+	);
+};
+
+export default Card;
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import banner from "../../img/Star-wars-logo.png"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+		<nav className="navbar bg-body-tertiary d-flex">
+			<div class="container-fluid">
+				<Link to="/">
+					<img className="navbar-brand mb-0 h1" src={banner}/>
 				</Link>
+
+				<Link to="demo">
+					<div class="btn-group">
+						<button type="button" class="btn btn-primary">Favorites</button>
+						<button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+							<span class="visually-hidden">Toggle Dropdown</span>
+						</button>
+						<ul class="dropdown-menu">
+							<li class="dropdown-item">Something else here</li>
+							<li class="dropdown-item">Something else here</li>
+							<li class="dropdown-item">Something else here</li>
+							<li class="dropdown-divider"></li>
+							<li class="dropdown-item" >Separated link</li>
+						</ul>
+					</div>
+				</Link>
+				
+							
 			</div>
+			
 		</nav>
+
+
+
+
 	);
 };
