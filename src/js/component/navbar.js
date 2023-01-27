1bbf2dccdbed873fd27c99b1f683b8b22d3405fa
Blancreyes <luijbr83@gmail.com>
2023-01-27 12:32:28 +0000

    Fixing NavBar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,32 +4,39 @@ import banner from "../../img/Star-wars-logo.png"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar bg-body-tertiary d-flex justify-content-between">
-			<div class="container">
-				<Link to="/">
-					<img className="navbar-brand mb-0 h1" src={banner}/>
-				</Link>
 
-				<Link to="demo">
-					<div class="btn-group">
-						<button type="button" class="btn btn-primary">Favorites</button>
-						<button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-							<span class="visually-hidden">Toggle Dropdown</span>
-						</button>
-						<ul class="dropdown-menu">
-							<li class="dropdown-item">Something else here</li>
-							<li class="dropdown-item">Something else here</li>
-							<li class="dropdown-item">Something else here</li>
-							<li class="dropdown-divider"></li>
-							<li class="dropdown-item" >Separated link</li>
-						</ul>
-					</div>
-				</Link>
-				
+		
+			<nav className="navbar bg-body-tertiary d-flex justify-content-between">
+				<div class="container">
+					<Link to="/">
+						<span className="navbar-brand mb-0">
+							<img className="object-fit-contain" style={{height: "100px"}} src="https://www.aaddpa.org/ckfinder/userfiles/files/starwars.png"/>
+						</span>
 							
-			</div>
+					</Link>
+
+					<Link to="demo">
+						<div class="btn-group">
+							<button type="button" class="btn btn-primary">Favorites</button>
+							<button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+								<span class="visually-hidden">Toggle Dropdown</span>
+							</button>
+							<ul class="dropdown-menu">
+								<li class="dropdown-item">Something else here</li>
+								<li class="dropdown-item">Something else here</li>
+								<li class="dropdown-item">Something else here</li>
+								<li class="dropdown-divider"></li>
+								<li class="dropdown-item" >Separated link</li>
+							</ul>
+						</div>
+					</Link>
+					
+								
+				</div>
+				
+			</nav>
+		
 			
-		</nav>
 
 
 
