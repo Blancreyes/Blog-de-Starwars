4cc439eb89c82c08d75045388b631d16152fefa6
Blancreyes <luijbr83@gmail.com>
2023-01-30 16:12:40 +0000

    Adding

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -2,22 +2,42 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import LearMore from "../component/learnMore.jsx";
 
 export const Single = props => {
 	const { store, actions } = useContext(Context);
-	const params = useParams();
+	const {type, theid} = useParams();
+	
+
+	function getInfo () {
+
+        https://swapi.dev/api/people
+        fetch(`https://www.swapi.tech/api/${type}/${theid}`)
+        .then(res => res.json())
+		.then(data => console.log(data))
+		.catch(err => console.error(err))
+        
+    }
+
+	useEffect(()=>{
+		getInfo()
+	},[])
+
 	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
+		<>
+			<LearMore
+				
+			/>
 
-			<hr className="my-4" />
-
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
+			<div className="py-2">
+				<Link to="/">
+					<span className="btn btn-primary btn-lg" href="#" role="button">
+						Back home
+					</span>
+				</Link>
+			</div>
+				
+		</>
 	);
 };
 
